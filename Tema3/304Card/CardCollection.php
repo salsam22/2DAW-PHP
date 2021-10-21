6af479147adf1bea3895b9c25f3cce6b6896f2b0
salsam22 <76152023+salsam22@users.noreply.github.com>
2021-10-21 21:42:16 +0200

    303 Card

--- Tema3/304Card/CardCollection.php
+++ Tema3/304Card/CardCollection.php
@@ -44,11 +44,10 @@ class CardCollection
         }
     }
 
-    public function deal():array
+    public function deal(int $amount):array
     {
-        //echo "<img alt=\"imagens\" src=\"cards/" . $this->cards[$rand]->getSymbol() . "_of_" . $this->cards[$rand]->getSuit() . ".svg\"/>";
         $cartesAJugar = [];
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < $amount; $i++) {
             $rand = array_rand($this->cards,1);
 
             array_push($cartesAJugar, $this->cards[$rand]);
@@ -58,10 +57,44 @@ class CardCollection
     }
 
     public function play() {
-        $array = $this->deal();
-        var_dump(array_chunk($array,5));
+        $jugador1 = [];
+        $jugador2 = [];
+        $jugador1 = $this->deal(5);
+        $jugador2 = $this->deal(5);
+        $cartesJugador1 = [];
+        $cartesJugador2 = [];
+        $vicJug1 = 0;
+        $vicJug2 = 0;
+        echo "<table border='solid 1px black'><tr>";
         for ($i = 0; $i < 5;$i++) {
-            echo "<img alt=\"imagens\" src=\"cards/" . $array[0][$i]->getSymbol() . "_of_" . $array[0][$i]->getSuit() . ".svg\"/>";
+            echo "<td><img alt=\"imagens\" src=\"cards/" . $jugador1[$i]->getSymbol() . "_of_" . $jugador1[$i]->getSuit() . ".svg\"/></td>";
+            array_push($cartesJugador1, $jugador1[$i]->getValue());
+        }
+        echo "</tr><tr>";
+        for ($i = 0; $i < 5;$i++) {
+            echo "<td><img alt=\"imagens\" src=\"cards/" . $jugador2[$i]->getSymbol() . "_of_" . $jugador2[$i]->getSuit() . ".svg\"/></td>";
+            array_push($cartesJugador2, $jugador2[$i]->getValue());
+        }
+        echo "</tr><tr>";
+
+        for ($i = 0; $i < 5;$i++) {
+            if ($cartesJugador1[$i] < $cartesJugador2[$i]) {
+                echo "<td>Jugador 2 guanya</td>";
+                $vicJug2++;
+            } else if ($cartesJugador1[$i] > $cartesJugador2[$i]) {
+                echo "<td>Jugador 1 guanya</td>";
+                $vicJug1++;
+            } else if ($cartesJugador1[$i] == $cartesJugador2[$i]) {
+                echo "<td>Empate</td>";
+            }
+        }
+        echo "</tr></table>";
+        if ($vicJug1 > $vicJug2){
+            echo "<p>Ha guanyat la partida el jugador 1</p>";
+        } else if ($vicJug1 < $vicJug2){
+            echo "<p>Ha guanyat la partida el jugador 2</p>";
+        } else if ($vicJug1 == $vicJug2){
+            echo "<p>La partida ha quedat empate</p>";
         }
     }
 
